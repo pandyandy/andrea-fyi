e7777d60b77d0019e298d0e541fe7f7036719ae4
pandyandy <and.reanovakova@seznam.cz>
2025-12-22 22:40:48 +0100

    Remove .svelte-kit from repository (should be ignored)

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/article/tobe-article/+page.svelte";
--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DHZuK843.js","_app/immutable/chunks/BvRgzv0k.js","_app/immutable/chunks/IHki7fMi.js"];
-export const stylesheets = ["_app/immutable/assets/0.DtwsbTaZ.css"];
-export const fonts = [];
